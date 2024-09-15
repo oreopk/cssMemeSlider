e84d365733067096bfcf251a6f79f625deca1fe8
PAVEL KOZIN <sart6zaV@yandex.ru>
2024-09-16 01:09:53 +0300

    fix: desktop cssMemeSlider-task (Mon, Sep 16, 2024 01:09:53 AM)

--- cssMemeSlider/script.js
+++ cssMemeSlider/script.js
@@ -1,15 +1,23 @@
 let currentSlide = 0;
 
-function showSlide(index) {
+function showSlide(index,btn1) {
     const slides = document.querySelector('.slides');
     currentSlide = index;
     const offset = -currentSlide * 100; // Вычислить смещение слайдов
     slides.style.transform = `translateX(${offset}%)`; // Переместить слайды
+deleteActive(btn1);
 }
 document.addEventListener("DOMContentLoaded", function() {
-  const btns = document.querySelectorAll('.btn');
-console.log(btns);
+  const btns = document.querySelectorAll('.button');
   btns.forEach((btn,index) => {
-    btn.addEventListener('click',  () => showSlide(index));
+    btn.addEventListener('click',  () => showSlide(index,btn));
 });
 });
+function deleteActive(btn1)
+{
+  const btns = document.querySelectorAll('.button');
+  btns.forEach((btn,index) => {
+    btn.classList.remove('active');
+});
+btn1.classList.add('active');
+}
