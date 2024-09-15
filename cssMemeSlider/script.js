bfe7a304747f06107da137f0d264f3dc61773e87
PAVEL KOZIN <sart6zaV@yandex.ru>
2024-09-16 01:34:24 +0300

    fix: scale  cssMemeSlider-task (Mon, Sep 16, 2024 01:34:24 AM)

--- cssMemeSlider/script.js
+++ cssMemeSlider/script.js
@@ -5,19 +5,38 @@ function showSlide(index,btn1) {
     currentSlide = index;
     const offset = -currentSlide * 100; // Вычислить смещение слайдов
     slides.style.transform = `translateX(${offset}%)`; // Переместить слайды
-deleteActive(btn1);
+Active(btn1);
+ActiveSub(index);
 }
 document.addEventListener("DOMContentLoaded", function() {
+  const subs = document.querySelectorAll('.sub');
+  subs.forEach((sub,index) => {
+    sub.classList.add('disabled');
+    sub.classList.remove('active');
+});
+subs[0].classList.add('active');
+subs[0].classList.add('disabled');
+
   const btns = document.querySelectorAll('.button');
   btns.forEach((btn,index) => {
     btn.addEventListener('click',  () => showSlide(index,btn));
 });
 });
-function deleteActive(btn1)
+function Active(btn1)
 {
   const btns = document.querySelectorAll('.button');
   btns.forEach((btn,index) => {
     btn.classList.remove('active');
 });
 btn1.classList.add('active');
+}
+function ActiveSub(index)
+{
+  const subs = document.querySelectorAll('.sub');
+  subs.forEach((sub,index) => {
+    sub.classList.add('disabled');
+    sub.classList.remove('active');
+});
+subs[index].classList.add('active');
+subs[index].classList.add('disabled');
 }
